bb3e96125968d52e012095ee7c5ec4f883098b02
mahendraputra90 <mahendrakoyo90@gmail.com>
2022-04-28 23:22:45 +0800

    tambahan kode

--- app/Views/mantap.php
+++ app/Views/mantap.php
@@ -10,7 +10,28 @@
 
 <body>
 
-
+    <div class="container">
+        <div class="sidebar">
+            <nav>
+                <ul>
+                    <li><a href="https://www.instagram.com/digitalpicture_art/">My Design</a></li>
+                    <li><a href="https://goo.gl/maps/hdqRxEA6KMiUYqo16">Link Address</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="content">
+            <section class="hero">
+                <img src="<?= base_url('img/koyo.png'); ?>">
+                <div class="hero-content">
+                    <h1>I Made Mahendra Putra<br>Asal Sidakarya</h1>
+                    <p>Saya adalah mahasiswa UNDIKNAS jurusan Teknologi Informasi kelas sore
+                        <br>beralamat di Jl.Bedugul Sidakarya
+                        <br>Saya kuliah sambil bekerja di sebuah minimarket dekat rumah
+                        <br>Saya menyukai desain grafis
+                    </p>
+                    <a class="action-btn" href="https://undiknas.ac.id/">UNDIKNAS UNIVERSITY</a>
+                </div>
+            </section>
 
 
 </body>
